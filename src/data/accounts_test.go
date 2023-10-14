819e568dee86a712e66e05f466b1594e7b73627d
Carlos Henrique <chff7cb@users.noreply.github.com>
2023-10-14 18:42:20 -0300

    chore: add data layer testing

--- src/data/accounts_test.go
+++ src/data/accounts_test.go
@@ -2,21 +2,94 @@ package data_test
 
 import (
 	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/chff7cb/swissbank/core"
+	"github.com/chff7cb/swissbank/data"
+	"github.com/chff7cb/swissbank/mocks"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-func TestAccountsData_CreateAccount(t *testing.T) {
-	ctx := context.Background()
-	account := core.Account{
+// accountsDataTestSuite test suite for accounts data proxy
+type accountsDataTestSuite struct {
+	suite.Suite
+	driver      *mocks.DynamoDBAPI
+	proxy       core.AccountsDataProxy
+	accountData core.Account
+}
+
+// TestAccountsDataSuite entry function that will run our test suite
+func TestAccountsDataSuite(t *testing.T) {
+	suite.Run(t, new(accountsDataTestSuite))
+}
+
+// SetupTest setup test state data before running each test case
+func (s *accountsDataTestSuite) SetupTest() {
+	s.driver = mocks.NewDynamoDBAPI(s.T())
+	s.proxy = data.NewAccountsData(s.driver, "accounts")
+	s.accountData = core.Account{
 		AccountID:      uuid.NewString(),
 		DocumentNumber: "12312312312",
 	}
+}
 
-	repos := AccountsData{}
-	err := repos.CreateAccount(ctx, &account)
-	if err != nil {
-		t.Fatal(err)
+// TestNewAccountsData tests the instantiation of a new accounts data proxy
+func (s *accountsDataTestSuite) TestNewAccountsData() {
+	proxy := data.NewAccountsData(s.driver, "accounts")
+	assert.NotEqual(s.T(), nil, proxy)
+}
+
+// TestAccountsDataImpl_CreateAccount will test the creation of a new account and the output data
+func (s *accountsDataTestSuite) TestAccountsDataImpl_CreateAccount() {
+	s.driver.On("PutItem", mock.Anything).Return(nil, nil)
+
+	newAccount, err := s.proxy.CreateAccount(context.Background(), &s.accountData)
+
+	assert.Equal(s.T(), nil, err)
+	assert.Equal(s.T(), s.accountData.AccountID, newAccount.AccountID)
+	assert.Equal(s.T(), s.accountData.DocumentNumber, newAccount.DocumentNumber)
+}
+
+// TestAccountsDataImpl_CreateAccount2 tests the case of a PutItem error when creating a new account
+func (s *accountsDataTestSuite) TestAccountsDataImpl_CreateAccount2() {
+	putItemError := errors.New("putitem error")
+	s.driver.On("PutItem", mock.Anything).Return(nil, putItemError)
+
+	_, err := s.proxy.CreateAccount(context.Background(), &s.accountData)
+
+	assert.Equal(s.T(), putItemError, err)
+}
+
+// TestAccountsDataImpl_GetAccountByID will test the returned data of the requests account
+func (s *accountsDataTestSuite) TestAccountsDataImpl_GetAccountByID() {
+	getItemOutput := dynamodb.GetItemOutput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"AccountID":      {S: &s.accountData.AccountID},
+			"DocumentNumber": {S: &s.accountData.DocumentNumber},
+		},
 	}
+
+	s.driver.On("GetItem", mock.Anything).
+		Return(&getItemOutput, nil)
+
+	accountData, err := s.proxy.GetAccountByID(context.Background(), s.accountData.AccountID)
+
+	assert.Equal(s.T(), nil, err)
+	assert.Equal(s.T(), s.accountData.AccountID, accountData.AccountID)
+	assert.Equal(s.T(), s.accountData.DocumentNumber, accountData.DocumentNumber)
+}
+
+// TestAccountsDataImpl_GetAccountByID2 test when retrieving account data fails due to GetItem error
+func (s *accountsDataTestSuite) TestAccountsDataImpl_GetAccountByID2() {
+	getItemError := errors.New("getitem error")
+	s.driver.On("GetItem", mock.Anything).
+		Return(nil, getItemError)
+
+	_, err := s.proxy.GetAccountByID(context.Background(), s.accountData.AccountID)
+
+	assert.Equal(s.T(), getItemError, err)
 }
---
+++ src/data/transactions_test.go
@@ -0,0 +1,77 @@
+package data_test
+
+import (
+	"context"
+	"errors"
+	"github.com/chff7cb/swissbank/core"
+	"github.com/chff7cb/swissbank/data"
+	"github.com/chff7cb/swissbank/mocks"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type transactionsDataTestSuite struct {
+	suite.Suite
+	driver          *mocks.DynamoDBAPI
+	proxy           core.TransactionsDataProxy
+	transactionData core.Transaction
+}
+
+// TestTransactionsDataSuite entry function that will run our test suite
+func TestTransactionsDataSuite(t *testing.T) {
+	suite.Run(t, new(transactionsDataTestSuite))
+}
+
+// SetupTest setup test state data before running each test case
+func (s *transactionsDataTestSuite) SetupTest() {
+	s.driver = mocks.NewDynamoDBAPI(s.T())
+	s.proxy = data.NewTransactionsData(s.driver, "transactions")
+	s.transactionData = core.Transaction{
+		TransactionID:   uuid.NewString(),
+		AccountID:       uuid.NewString(),
+		Amount:          0.1 + 0.2,
+		OperationTypeID: 4,
+		EventTimestamp:  time.Time{},
+	}
+}
+
+// TestNewTransactionsData test the instantiation of a data proxy
+func (s *transactionsDataTestSuite) TestNewTransactionsData() {
+	proxy := data.NewTransactionsData(s.driver, "transactions")
+	assert.NotEqual(s.T(), nil, proxy)
+}
+
+// TestTransactionsDataImpl_CreateTransaction test the creating of a new transaction and assert output data values
+func (s *transactionsDataTestSuite) TestTransactionsDataImpl_CreateTransaction() {
+	ctx := context.Background()
+
+	s.driver.On("PutItem", mock.Anything).
+		Return(nil, nil)
+
+	newTransaction, err := s.proxy.CreateTransaction(ctx, &s.transactionData)
+
+	assert.Equal(s.T(), nil, err)
+	assert.Equal(s.T(), s.transactionData.TransactionID, newTransaction.TransactionID)
+	assert.Equal(s.T(), s.transactionData.AccountID, newTransaction.AccountID)
+	assert.Equal(s.T(), s.transactionData.Amount, newTransaction.Amount)
+	assert.Equal(s.T(), s.transactionData.OperationTypeID, newTransaction.OperationTypeID)
+	assert.Equal(s.T(), s.transactionData.EventTimestamp, newTransaction.EventTimestamp)
+}
+
+// TestTransactionsDataImpl_CreateTransaction2 test the failed creation of a transaction due to a database error
+func (s *transactionsDataTestSuite) TestTransactionsDataImpl_CreateTransaction2() {
+	ctx := context.Background()
+
+	putItemError := errors.New("putitem error")
+
+	s.driver.On("PutItem", mock.Anything).
+		Return(nil, putItemError)
+
+	_, err := s.proxy.CreateTransaction(ctx, &s.transactionData)
+
+	assert.Equal(s.T(), putItemError, err)
+}
